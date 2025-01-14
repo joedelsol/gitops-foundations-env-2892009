0e8d4d7f9480c4bec2f72e0390c32dc19841b1a5
Joe Delsol <joe@delsols.net>
2025-01-14 10:05:14 -0800

    Initial setup adding iismjd repo to env files.

--- setup.sh
+++ setup.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-find . -type f -exec sed -i 's/{dockerHubUsername}/'$1'/g' {} +
+#find . -type f -exec sed -i 's/iismjd/'$1'/g' {} +
+if [ -z "$1" ]; then
+    echo "Usage: $0 <dockerHubUsername>"
+    exit 1
+fi
+
+find . -type f -exec file --mime {} + | grep 'text/' | cut -d: -f1 | while read -r file; do
+    sed -i '' 's/iismjd/'"$1"'/g' "$file"
+
+done
